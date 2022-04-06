c9d324ed6684a77a9dc8f017456753a927a36c61
muthpeter <34248737+muthpeter@users.noreply.github.com>
2022-04-06 13:45:49 +0200

    added handlebars to render the main page insted of a simple index.html

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const cards = require('./routes/cards')
 const app = express()
 const PORT = process.env.PORT || 3000
 const path =require('path')
+const exphbs  = require('express-handlebars')
 
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
 
 
 //parse 
@@ -20,6 +23,6 @@ app.use('/cards',cards)
 //static path
 app.use(express.static(path.join(__dirname,'/public')))
 
-app.get('/', (req, res) => res.sendFile('index.html'))
+app.get('/', (req, res) => res.render('home'))
 app.listen(PORT)
   
